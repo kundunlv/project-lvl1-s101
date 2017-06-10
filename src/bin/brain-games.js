78ada49e54e397ae7cce6179f8762c1f9e2b389e
bagtim <tibogom@gmail.com>
2017-06-10 17:50:20 +0300

    2nd task fixing mistakes

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,5 @@
+#!/Users/bagos/Desktop/project-lvl1-s101/src/bin node
 
+import {func} from 'src/index.js';
 
-import <func> from 'src/index.js';
+func();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default func = () = {	
-	console.log("$ src/bin/brain-games.js");
 	console.log("Welcome to the Brain Games!");
-	const actual = readlineSync.question('May I have your name? ');
-	console.log('Hi ' + actual + '!');
-	
+	const yourName = readlineSync.question('May I have your name? ');
+	console.log(`Hi ${yourName}!`);
 };
